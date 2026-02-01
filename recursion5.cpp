cf1f54ac6e95ebb953b817731bcad3e5d80ab2bf
Saurabh singh <saurabh_kumar@srmap.edu.in>
2026-02-01 18:28:55 +0530

    commit

---
+++ recursion5.cpp
@@ -0,0 +1,16 @@
+//print a number from n to 1 using recusrion by backtrack method
+#include<bits/stdc++.h>
+using namespace std;
+void f(int i,int n){
+    if(i>n){
+        return;
+    }
+    f(i+1,n);
+    cout<<i<<endl;
+}
+int main(){
+    int n;
+    cout<<"Enter a number you want to start:- ";
+    cin>>n;
+    f(1,n);
+}
