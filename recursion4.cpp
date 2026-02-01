96b3ce583ea6879f93dbc4623444c35073a53296
Saurabh singh <saurabh_kumar@srmap.edu.in>
2026-02-01 18:17:20 +0530

    commit

---
+++ recursion4.cpp
@@ -0,0 +1,16 @@
+//print number from 1 to n using recursion by backtracking
+#include<bits/stdc++.h>
+using namespace std;
+void f(int i,int n){
+    if(i<1){
+        return;
+    }
+    f(i-1,n);
+    cout<<i<<endl;
+}
+int main(){
+    int n;
+    cout<<"enter a number:- ";
+    cin>>n;
+    f(n,n);
+}
